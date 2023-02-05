d8a9280e03bb9796f0728d2676919ced421249ed
Igor MGTOW <96206920+IgorMGTOW@users.noreply.github.com>
2023-02-05 12:54:32 +0100

    Add files via upload
    This application does the same thing as my 100 variables application did but it uses a while loop instead of manually adding 100 variables. it displays numbers 1-100 from top to bottom

---
+++ 100NumbersSecondEdition/100NumbersSecondEdition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()  // 1-100 numbers program second edition.
+{
+    int onehundred = 1;
+    while( onehundred < 101) {
+        printf("%d\n", onehundred);
+        onehundred++;
+    }
+    return 0;
+}
